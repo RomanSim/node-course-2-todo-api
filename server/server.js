62b0fbb2a76e762f3524173732cc6c82d8e8b48f
Roman Sima <romansima8@gmail.com>
2019-02-28 03:43:20 +0200

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -115,11 +115,6 @@ app.get('/users/me', authenticate, (req, res)=>{
    res.send(req.user);
 });
 
-app.listen(port, ()=>{
-  console.log(`Started up at port ${port}`)
-});
-
-// POST /users/login
 app.post('/users/login',(req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
 
@@ -130,6 +125,18 @@ app.post('/users/login',(req, res) => {
     }).catch((e) => {
         res.status(400).send(e);
     });
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    });
+});
+
+app.listen(port, ()=>{
+    console.log(`Started up at port ${port}`)
 });
 
 module.exports = {app};
